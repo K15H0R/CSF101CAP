0ca89f475d760794337eb7c650b7adac91643745
KISHOR KUMAR GHALLEY <02230266.cst@edu.bt>
2024-04-12 22:44:55 +0600

    CSF101

--- CAP-02230266.py
+++ CAP-02230266.py
@@ -23,7 +23,7 @@ def reader():
 def calculate_score(txt):
    
     totalscore = 0
-    store= {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7}
+    store= {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
     for round in txt: 
         score=round.strip()
         value=store.get(score,None)
@@ -34,7 +34,3 @@ def calculate_score(txt):
                 
 read_input()
 calculate_score(reader())
-
-
-
-

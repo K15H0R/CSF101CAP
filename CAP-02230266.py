a6ad03be17a1049d5071d997ae7bc338cf953ed0
KISHOR KUMAR GHALLEY <02230266.cst@edu.bt>
2024-04-13 10:21:35 +0600

    CSF101

--- CAP-02230266.py
+++ CAP-02230266.py
@@ -3,7 +3,7 @@
 # MECHANICAL ENGINEERING
 # 02230266
 ################################
-# REFERENCES
+# REFERENCES:
 # https://www.dataquest.io/blog/read-file-python/#:~:text=Python%20provides%20a%20built%2Din,we%20can%20manipulate%20its%20content
 # https://youtu.be/TSHEnfUz6wk?si=D6w-DN9uMU0GxUaV
 ################################
@@ -11,24 +11,24 @@
 # Your Solution Score:50223
 # number: 6
 ################################
-# Read the input.txt file
 def read_input():
     f = open('CSF101CAP/input_6_cap1.txt', 'r')
     print(f.read())
     f.close()
 
 def reader():
-    return open('CSF101CAP/input_6_cap1.txt', 'r')
+    A= open('CSF101CAP/input_6_cap1.txt', 'r')
+    return A
 
 def calculate_score(txt):
    
     totalscore = 0
-    store= {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
+    scorestore= {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
     for round in txt: 
         score=round.strip()
-        value=store.get(score,None)
-        if value is not None:
-            totalscore+=value
+        scorevalue=scorestore.get(score,None)
+        if scorevalue is not None:
+            totalscore+=scorevalue
 
     print("Total Score:", totalscore)
                 
